9632d01169190bb1f934a081fe866c7763b0e068
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-05-14 14:38:44 +0530

    Time: 64 ms (44.78%) | Memory: 61.4 MB (24.64%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int left=0, right=height.size()-1, ans=0;
+
+        while(left<right){
+            int area=min(height[left],height[right]) * (right-left);
+            ans=max(ans,area);
+
+            if(height[left]<height[right]) left++;
+            else right--;
+        }
+        return ans;
+    }
+};

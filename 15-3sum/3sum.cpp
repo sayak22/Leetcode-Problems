d4def0fdd41c057a43f4f1d567c5fb9981517e8a
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-05-18 18:30:26 +0530

    Time: 56 ms (84.46%) | Memory: 27.1 MB (80.43%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,17 +3,25 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
                int target = 0;
         sort(nums.begin(), nums.end());
-        set<vector<int>> s;
+        // set<vector<int>> s;
         vector<vector<int>> output;
+
         for (int i = 0; i < nums.size(); i++){
             int j = i + 1;
             int k = nums.size() - 1;
+
+            //skipping duplicate values of i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
+                    output.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+
+                    while (j < k && nums[k] == nums[k + 1]) k--;
                 } else if (sum < target) {
                     j++;
                 } else {
@@ -21,8 +29,8 @@ public:
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
+        // for(auto triplets : s)
+        //     output.push_back(triplets);
         return output;
     }
 };

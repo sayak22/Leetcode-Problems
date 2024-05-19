099d4eb9e738373cd053d833e67982e84c37d9f3
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-05-20 01:51:37 +0530

    Time: 8 ms (6.48%) | Memory: 15.1 MB (5.00%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+vector<int> dp;
+
+    int solve(int i, string s){
+        //base cases
+        if(i >= s.size()) return 1;
+        else if(s[i]=='0') return 0;
+        else if(i==s.size()-1) return 1;
+        //body
+        else if(dp[i]!=-1) return dp[i];
+        else if(s[i]=='1' or (s[i]=='2' and s[i+1]<='6'))
+        return dp[i]= solve(i+1,s) + solve(i+2,s);
+        else
+        return dp[i]=solve(i+1,s);
+
+    }
+
+    int numDecodings(string s) {
+        dp.resize(s.size());
+        dp.assign(s.size(),-1);
+          return solve(0,s);
+    }
+};

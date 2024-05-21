dfbc3c84b38a6c63b837a165cabde0bfd41aa87e
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-05-22 00:55:19 +0530

    Time: 3 ms (29.05%) | Memory: 7.3 MB (49.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/* RECCURSION + MEMORISATION
     vector<int> dp;
     int solve(int n){
          if (n==1 || n==0) return 1;
@@ -10,5 +11,17 @@ public:
         dp.resize(n+1);
         dp.assign(n+1,-1);
         return solve(n);
+    }
+    */
+    vector<int> dp;
+    int climbStairs(int n) {
+        if(n==0 || n==1) return 1;
+        dp.resize(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };

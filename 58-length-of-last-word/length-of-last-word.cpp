96cd036956c00a3a15aec0ed325a7ab9d37445a4
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-05-27 06:25:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (55.70%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.length() - 1;
-        // eliminating all space at the end to get to the last character of the last word
-        while(s[n] == ' ')
-            n--;
-        int ans = 0;
-        //counting the number of letters in the word, i.e, untill the current character is a space, we count the no. of characters which is = the length of the last word.
-        while(n >= 0 && s[n] != ' ') {
-            n--;
-            ans++;
+        // int n = s.length() - 1;
+        // // eliminating all space at the end to get to the last character of the last word
+        // while(s[n] == ' ')
+        //     n--;
+        // int ans = 0;
+        // //counting the number of letters in the word, i.e, untill the current character is a space, we count the no. of characters which is = the length of the last word.
+        // while(n >= 0 && s[n] != ' ') {
+        //     n--;
+        //     ans++;
+        // }
+        // return ans;
+
+        int n=s.length(), count=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==' ') continue;
+            else{
+                while((i<n &&i>=0) && s[i]!=' '){
+                    count++;
+                    i--;
+                }
+                return count;
+            }
         }
-        return ans;
+        return 0;
     }
 };

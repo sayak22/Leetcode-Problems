7fc27b9f41fdc67741b4138d6f33fce0899fd0a6
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-06-08 05:26:38 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (81.46%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(vector<int>& nums, int n) {
-        if (n <= 0)
-            return 0;
+/*
+::::::::::::::::::::::::::
+RECCURSION + MEMORIZATION
+::::::::::::::::::::::::::
+*/
+    // vector<int> dp;
+    // int solve(vector<int>& nums, int n) {
+    //     if (n <= 0)
+    //         return 0;
 
-        if (dp[n] != -1)
-            return dp[n];
-        //        max(NOT taking current element, taking current element)
-        return dp[n] = max(solve(nums, n - 1), nums[n - 1] + solve(nums, n - 2));
-    }
+    //     if (dp[n] != -1)
+    //         return dp[n];
+    //     //        max(NOT taking current element, taking current element)
+    //     return dp[n] = max(solve(nums, n - 1), nums[n - 1] + solve(nums, n - 2));
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     dp.assign(nums.size() + 1, -1);
+    //     return solve(nums, nums.size());
+    // }
 
     int rob(vector<int>& nums) {
-        dp.assign(nums.size() + 1, -1);
-        return solve(nums, nums.size());
+        int prev2=0, prev=0, curi; //prev2 represents dp[-ve index], prev represents dp[0], curri represents dp[i]
+
+        for(int i=0;i< nums.size();i++){
+            int take = nums[i] + prev2;
+            int notTake = 0 + prev;
+            int curi=max(take, notTake);
+            prev2=prev;
+            prev=curi;
+        }
+        return prev;
     }
 };

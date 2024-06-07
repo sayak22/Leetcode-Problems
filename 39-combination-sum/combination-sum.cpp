937c285b3f00799aa3a0a88e3d382faf70b077b5
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-06-07 05:47:52 +0530

    Time: 8 ms (47.27%) | Memory: 12.4 MB (55.82%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -4,17 +4,18 @@ public:
 
     void solve(int index, int target, vector<int>& candidates, vector<int>& ds) {
         if (index >= candidates.size()) {
-            if (target == 0)
+            if (target == 0) // target = 0 means ds has one of the combination
                 ans.push_back(ds);
-                return;
+                return;// returns, irrespective of whether target is 0 or not(preventing out of bounds problem)
         }
 
+        // if the entire array isn't traversed yet, then we must make sure that cur element <= target
         if (target >= candidates[index]) {
             ds.push_back(candidates[index]);
-            solve(index, target - candidates[index], candidates, ds);
-            ds.pop_back();
+            solve(index, target - candidates[index], candidates, ds);// deducting from target and considering the same element again, untill the target becomes less than that element.
+            ds.pop_back(); // while backtracking from the decision tree, we need to remove the last element, as we are rejecting the current element and moving forward to the next element.
         }
-        solve(index + 1, target, candidates, ds);
+        solve(index + 1, target, candidates, ds); // calling for the next element in the array.
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> ds; // data structure to store the combinations

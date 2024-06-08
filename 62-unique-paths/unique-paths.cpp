733bd40cd7593660869f7670b3dab1578d1f0586
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-06-09 04:58:36 +0530

    Time: 2 ms (42.09%) | Memory: 7.2 MB (86.48%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int i, int j, int m, int n) {
-        if (i == m - 1 && j == n - 1)
-            return 1;
-        else if (i >= m || j >= n)
-            return 0;
+    /*
+    ::::::::::::::::::: RECCURSION AND MEMORISATION :::::::::::::::::
+    */
+    // vector<vector<int>> dp;
+    // int solve(int i, int j, int m, int n) {
+    //     if (i == m - 1 && j == n - 1)
+    //         return 1;
+    //     else if (i >= m || j >= n)
+    //         return 0;
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        else
-            return dp[i][j] = solve(i + 1, j, m, n) + solve(i, j + 1, m, n);
-    }
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     else
+    //         return dp[i][j] = solve(i + 1, j, m, n) + solve(i, j + 1, m, n);
+    // }
 
+    // int uniquePaths(int m, int n) {
+    //     dp.assign(m + 1, vector<int>(n + 1, -1));
+    //     return solve(0, 0, m, n);
+    // }
+
+    /*
+    ::::::::::::::::::: COMBINATRICS :::::::::::::::::
+    */
     int uniquePaths(int m, int n) {
-        dp.assign(m + 1, vector<int>(n + 1, -1));
-        return solve(0, 0, m, n);
+        int N = m + n - 2;
+        // since total right moves = n-1  and total down moves = m-1, we take the sum of them.
+        int r = n - 1 ;// or r = n-1 (both will give same answers)
+                /*
+                Explaination: if out of the total no. of moves we take all the
+                right moves in any order then there is only one way for all the
+                down moves to order themselves. and vice versa, therefore,
+                taking m-1 or n-1 as r will give the same answer.
+                */
+        double ans = 1;
+        for (int i = 1; i <= r; i++) 
+            ans = ans * (N - r + i) / i;
+        
+        return (int)ans;
     }
 };

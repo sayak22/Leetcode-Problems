431f55db9e7588dfc02e592945d177efefbe59ad
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-06-09 05:47:34 +0530

    Time: 50 ms (34.03%) | Memory: 50.7 MB (81.23%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool solve(vector<int>& nums, int n) {
-        int index = n - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (i + nums[i] >= index)
-                index = i;
+        int goal = n - 1; // placing our goal at the last position
+        for (int i = n - 1; i >= 0; i--) { // running a reverse loop
+            if (i + nums[i] >=goal) // if the current element be added to current index, can
+                                    // it reach or surpass the goal?
+                goal = i; // if yes then we update our goal to the current index
+                          // as if we can reach the current index, we can reach
+                          // the end as well, if not we move to the next index
+                          // and check the same again.
         }
-        if (index == 0)
+        // the goal has shifted to the first element (position we start from)
+        // then we can reach our original goal.
+        if (goal == 0)
             return true;
         return false;
     }

981ae1d53382e425517c0e7f37243db64ae91d34
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-06-09 05:43:18 +0530

    Time: 37 ms (87.41%) | Memory: 50.9 MB (50.34%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-
-    bool solve(vector<int>& nums, int n){
-        if(n==0) return true;
-        int index=n-1;
-        for(int i=n-1 ;i>=0;i--){
-            if(i+nums[i]>=index)
-                index=i;
-            
+    bool solve(vector<int>& nums, int n) {
+        int index = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + nums[i] >= index)
+                index = i;
         }
-            if (index==0) return true;
-            return false;
+        if (index == 0)
+            return true;
+        return false;
     }
 
     bool canJump(vector<int>& nums) {
-    // int ans = solve(nums, 0,nums.size());
-    // return ans==1;
-    return solve(nums, nums.size());
+        // int ans = solve(nums, 0,nums.size());
+        // return ans==1;
+        return solve(nums, nums.size());
     }
 };

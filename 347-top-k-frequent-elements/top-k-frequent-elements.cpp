83f60458ad3e4c0550942dd76baf52867f87d7fa
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-06-28 18:01:02 +0530

    Time: 4 ms (97.86%) | Memory: 19 MB (7.23%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -12,8 +12,9 @@ public:
         for (auto i : mp)
             v[i.second].push_back(i.first);
 
-        for (int i = v.size() - 1; i >= 0 && ans.size() < k; i--) {
-            for (auto j : v[i]) {
+        for (auto i = v.end(); i != v.begin();) {
+            --i;
+            for (auto j : *i) {
                 ans.push_back(j);
                 if (ans.size() == k)
                     return ans;

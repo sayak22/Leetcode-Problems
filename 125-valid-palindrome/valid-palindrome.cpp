56cb9739cd78b944fbf599cb29c52e46f7afadcf
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-02 03:01:13 +0530

    Time: 4 ms (79.71%) | Memory: 8.6 MB (83.41%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int front = 0, back = s.size() - 1;
+        while (front <= back) {
+            if (!isalnum(s[front])) {
+                front++;
+                continue;
+            }
+            if (!isalnum(s[back])) {
+                back--;
+                continue;
+            }
+
+            if (tolower(s[front]) != tolower(s[back]))
+                return false;
+            else {
+                front++;
+                back--;
+            }
+        }
+        return true;
+    }
+};

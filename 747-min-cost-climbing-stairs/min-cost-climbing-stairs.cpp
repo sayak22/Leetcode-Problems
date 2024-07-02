3615bb2bb712749e7dc889a2ef93db9b20d33d3e
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-03 05:04:53 +0530

    Time: 10 ms (15.12%) | Memory: 16.4 MB (19.14%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,17 +1,33 @@
+
 class Solution {
 public:
-    int helper(vector<int>& cost, int i, vector<int>& dp) {
-        if (i >= cost.size())
-            return 0;
+    /*
+    ::::::::::::::::::::::::::::
+    Recursion + memorisation :::
+    ::::::::::::::::::::::::::::
+    */
+    // int helper(vector<int>& cost, int i, vector<int>& dp) {
+    //     if (i >= cost.size())
+    //         return 0;
 
-        if (dp[i] != -1)
-            return dp[i];
+    //     if (dp[i] != -1)
+    //         return dp[i];
 
-        return cost[i] + min(dp[i + 1] = helper(cost, i + 1, dp),
-                             dp[i + 2] = helper(cost, i + 2, dp));
-    }
+    //     return cost[i] + min(dp[i + 1] = helper(cost, i + 1, dp),
+    //                          dp[i + 2] = helper(cost, i + 2, dp));
+    // }
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     vector<int> dp(cost.size() + 2, -1);
+    //     return min(helper(cost, 0, dp), helper(cost, 1, dp));
+    // }
+
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size() + 2, -1);
-        return min(helper(cost, 0, dp), helper(cost, 1, dp));
+        int n = cost.size();
+        vector<int> dp(n + 1, 0);
+
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2]);
+        }
+        return dp[n];
     }
 };

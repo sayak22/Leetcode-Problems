fc458efdcf5ff7358433b9816e03573f95ec9414
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-03 05:07:13 +0530

    Time: 3 ms (79.99%) | Memory: 16.4 MB (40.16%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -6,28 +6,33 @@ public:
     Recursion + memorisation :::
     ::::::::::::::::::::::::::::
     */
-    // int helper(vector<int>& cost, int i, vector<int>& dp) {
-    //     if (i >= cost.size())
-    //         return 0;
+    int helper(vector<int>& cost, int i, vector<int>& dp) {
+        if (i >= cost.size())
+            return 0;
 
-    //     if (dp[i] != -1)
-    //         return dp[i];
+        if (dp[i] != -1)
+            return dp[i];
 
-    //     return cost[i] + min(dp[i + 1] = helper(cost, i + 1, dp),
-    //                          dp[i + 2] = helper(cost, i + 2, dp)); // adding the current cost to the value of cost of the remaining array
-    // }
-    // int minCostClimbingStairs(vector<int>& cost) {
-    //     vector<int> dp(cost.size() + 2, -1);
-    //     return min(helper(cost, 0, dp), helper(cost, 1, dp));
-    // }
-
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1, 0);
-
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2]);
-        }
-        return dp[n];
+        return cost[i] + min(dp[i + 1] = helper(cost, i + 1, dp),
+                             dp[i + 2] = helper(cost, i + 2, dp)); // adding the current cost to the value of cost of the remaining array
     }
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size() + 2, -1);
+        return min(helper(cost, 0, dp), helper(cost, 1, dp));
+    }
+
+    /*
+    ::::::::::::::
+    Tabulation :::
+    ::::::::::::::
+    */
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     int n = cost.size();
+    //     vector<int> dp(n + 1, 0);
+
+    //     for (int i = 2; i <= n; ++i) {
+    //         dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2]);
+    //     }
+    //     return dp[n];
+    // }
 };

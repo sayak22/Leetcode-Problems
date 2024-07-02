e1c5e33eac4ff63b6644e8dcacb8c80590d0cd68
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-03 02:55:54 +0530

    Time: 9 ms (67.88%) | Memory: 23.1 MB (28.10%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,10 +3,13 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         int water = 0;
-        vector<int> left(n, 0);
-        vector<int> right(n, 0);
-        left[0]=height[0];
-        right[n-1]=height[n-1];
+        vector<int> left(n, 0); // stores max height recorded to the left of the current element
+        vector<int> right(n, 0);// stores max height recorded to the right of the current element
+        //we donot need the end elements as they cannot store water (wall on only one side)
+        left[0]=height[0]; //assigning this because for left[1] the max element to the left element be the height of that element
+        right[n-1]=height[n-1]; // same reason as to why we assigned left[0] = height[0]
+
+
         for (int i = 1; i < n-1; i++)
             left[i] = max(left[i-1], height[i]);
 
@@ -14,8 +17,8 @@ public:
             right[i] = max(right[i+1], height[i]);
 
         for (int i = 1; i < n - 1; i++) {
-            if (height[i] < right[i] and height[i] < left[i])
-                water += min(left[i], right[i]) - height[i];
+            if (height[i] < right[i] and height[i] < left[i]) // that element can store water only if it has height less than the highest height to it's right and left
+                water += min(left[i], right[i]) - height[i]; //water level will be till the minimum of the wall height to the right and left
         }
         return water;
     }

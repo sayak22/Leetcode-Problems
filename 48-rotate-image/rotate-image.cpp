1dd546c7371dea0bb86e52b218155b23b53e3bf7
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-03 20:06:07 +0530

    Time: 5 ms (5.40%) | Memory: 8.7 MB (22.47%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        // to rotate any 2D array by 90 degree, just do transpose and then mirror horizontally (reverse the collumns)
+
+        
+        // transposing the matrix
         for (int i = 0; i < matrix.size(); i++)
             for (int j = 0; j < matrix[i].size(); j++)
                 if (i > j)
                     swap(matrix[i][j], matrix[j][i]);
 
+
+        // reversing the collumns 
         for (int i = 0; i < matrix.size(); i++)
             reverse(matrix[i].begin(), matrix[i].end());
     }

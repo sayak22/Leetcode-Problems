c4f295ec28f18f1c5b0189d4ac305c4ce51d863b
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-11 21:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (70.28%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-
-        // In this we are storing the indexes of the starting parenthesis '('
-        // and whenever we encounter a ')' we are reversing the string inside
-        // the last '(' and current ')'
+        /*
+        In this we are storing the indexes of the starting parenthesis '(' and
+        whenever we encounter a ')' we are reversing the string inside the last
+        '(' and current ')'
+        */
         stack<int> st;
         for (int i = 0; i < s.length(); i++) {
             if (s[i] == '(')
                 st.push(i);
             else if (s[i] == ')') {
-                int start =
-                    st.top() +
-                    1; // since st.top() gives index of last '(' encountered,
-                       // the word will start from the next index
-                reverse(s.begin() + start,
-                        s.begin() +
-                            i); // reverse(starting pointer, ending pointer)
+                int start = st.top() + 1; // since st.top() gives index of last '(' encountered, the word will start from the next index
+                reverse(s.begin() + start, s.begin() + i); // reverse(starting pointer, ending pointer)
                 st.pop();
             }
         }

b2d90e39e33ef717490df6527dcf1a637addb5db
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-13 18:49:30 +0530

    Time: 9 ms (62.21%) | Memory: 14.6 MB (35.56%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
-    int countSubsetSum(vector<int>& nums, int sum, int n) {
-        if (n == 0 and sum > 0)
-            return 0;
-        else if (n == 1) {
-            if (sum == 0)
-                return (nums[0] == 0) ? 2 : 1;
-            else
-                return nums[0] == sum;
-        }
-        else if (n==0 and sum==0) return 1; 
 
-        int notTake = countSubsetSum(nums, sum, n - 1);
-        int take = 0;
-        if (nums[n-1] <= sum)
-            take = countSubsetSum(nums, sum - nums[n-1], n - 1);
-        return notTake + take;
+// ::::::::::::::::::::::::::::
+// :::::: RECURSION :::::::::::
+// ::::::::::::::::::::::::::::
+    // int countSubsetSum(vector<int>& nums, int sum, int n) {
+    //     if (n == 0 and sum > 0)
+    //         return 0;
+    //     else if (n == 1) {
+    //         if (sum == 0)
+    //             return (nums[0] == 0) ? 2 : 1;
+    //         else
+    //             return nums[0] == sum;
+    //     }
+    //     else if (n==0 and sum==0) return 1; 
+
+    //     int notTake = countSubsetSum(nums, sum, n - 1);
+    //     int take = 0;
+    //     if (nums[n-1] <= sum)
+    //         take = countSubsetSum(nums, sum - nums[n-1], n - 1);
+    //     return notTake + take;
+    // }
+
+        int countSubsetSum(vector<int>& nums, int sum, int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+
+        // Initialization
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1; // There's one way to get sum 0: by choosing no elements
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                dp[i][j] = dp[i - 1][j]; // notTake case
+                if (nums[i - 1] <= j) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]]; // take case
+                }
+            }
+        }
+
+        return dp[n][sum];
     }
+
 
     int findTargetSumWays(vector<int>& nums, int target) {
         // watch video of aditya verma, very simple

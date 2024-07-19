aa92344c8319bd04b96b02543a70b1c6050bf161
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-19 18:01:04 +0530

    Time: 4 ms (70.07%) | Memory: 12.3 MB (17.02%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // Hash map to store the next greater element for each element in nums2
+        unordered_map<int, int> nextGreater;
+        // Stack to keep track of elements for which we need to find the next
+        // greater element
+        stack<int> s;
+
+        // Iterate through nums2 to fill the nextGreater map
+        for (int num : nums2) {
+            // While stack is not empty and the current element is greater than
+            // the element at the top of the stack
+            while (!s.empty() && s.top() < num) {
+                // The current element is the next greater element for the
+                // element at the top of the stack
+                nextGreater[s.top()] = num;
+                // Pop the top element from the stack
+                s.pop();
+            }
+            // Push the current element onto the stack
+            s.push(num);
+        }
+
+        // For elements that do not have a next greater element, set their next
+        // greater element to -1
+        while (!s.empty()) {
+            nextGreater[s.top()] = -1;
+            s.pop();
+        }
+
+        // Prepare the result for nums1
+        vector<int> result;
+        for (int num : nums1) {
+            // Use the nextGreater map to find the next greater element for each
+            // element in nums1
+            result.push_back(nextGreater[num]);
+        }
+
+        return result;
+    }
+};

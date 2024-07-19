049edbf2c4ebf4a2d9923aef286d2cf24b928145
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-19 19:07:24 +0530

    Time: 7 ms (87.27%) | Memory: 23.5 MB (9.86%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
+
+  // TC: O(N) SC:O(N) n is the number of blocks whose heights are present in the array
     int trap(vector<int>& height) {
         int n = height.size();
         int water = 0;
-        vector<int> left(n, 0); // stores max height recorded to the left of the current element
-        vector<int> right(n, 0);// stores max height recorded to the right of the current element
-        //we donot need the end elements as they cannot store water (wall on only one side)
-        left[0]=height[0]; //assigning this because for left[1] the max element to the left element be the height of that element
-        right[n-1]=height[n-1]; // same reason as to why we assigned left[0] = height[0]
+        // Arrays to store the maximum height to the left and right of each
+        // element
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
 
+        // assigning this because for left[1] the max element to the left
+        // element be the height of that element
+        left[0] = height[0];
+        // same reason as to why we assigned left[0] = height[0]
+        right[n - 1] = height[n - 1];
 
-        for (int i = 1; i < n-1; i++)
-            left[i] = max(left[i-1], height[i]);
+        // Fill the left array with the maximum height to the left of each
+        // element
+        for (int i = 1; i < n - 1; i++)
+            left[i] = max(left[i - 1], height[i]);
 
+        // Fill the right array with the maximum height to the right of each
+        // element
         for (int i = n - 2; i >= 1; i--)
-            right[i] = max(right[i+1], height[i]);
+            right[i] = max(right[i + 1], height[i]);
 
+        // Calculate the trapped water
         for (int i = 1; i < n - 1; i++) {
-            if (height[i] < right[i] and height[i] < left[i]) // that element can store water only if it has height less than the highest height to it's right and left
-                water += min(left[i], right[i]) - height[i]; //water level will be till the minimum of the wall height to the right and left
+            // Water can be trapped only if the current height is less than both
+            // left and right heights
+            if (height[i] < right[i] && height[i] < left[i]) {
+                // The water level is determined by the shorter of the two walls
+                // (left and right)
+                water += min(left[i], right[i]) - height[i];
+            }
         }
         return water;
     }

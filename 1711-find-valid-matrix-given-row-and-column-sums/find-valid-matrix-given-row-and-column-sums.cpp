426a6fe9fb1bdca36b773a9e5c2f6e9c2d9882f3
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-07-20 17:17:44 +0530

    Time: 31 ms (88.26%) | Memory: 36.1 MB (11.51%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        const int m = rowSum.size();  // Number of rows
-        const int n = colSum.size();  // Number of columns
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum,
+                                      vector<int>& colSum) {
+        const int m = rowSum.size(); // Number of rows
+        const int n = colSum.size(); // Number of columns
 
         // Initialize an empty matrix (2D vector) with zeros
         vector<vector<int>> ans(m, vector<int>(n, 0));
@@ -11,7 +12,8 @@ public:
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 // Calculate the value to place in the cell
-                // It's the minimum of remaining row sum for row 'i' and remaining column sum for column 'j'
+                // It's the minimum of remaining row sum for row 'i' and
+                // remaining column sum for column 'j'
                 int value = min(rowSum[i], colSum[j]);
 
                 // Update the cell value
@@ -22,6 +24,7 @@ public:
                 colSum[j] -= value;
 
                 // If both rowSum[i] and colSum[j] become zero, terminate early
+                // (Saves a bit more time)
                 if (rowSum[i] == 0 && colSum[j] == 0)
                     break;
             }

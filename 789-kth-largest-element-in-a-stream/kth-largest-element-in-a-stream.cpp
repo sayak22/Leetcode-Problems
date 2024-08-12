0933f8ea394a279428d31aee99febb0e1ebf4dda
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-08-13 02:32:06 +0530

    Time: 36 ms (6.99%) | Memory: 24.8 MB (17.50%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,16 +1,16 @@
 class KthLargest {
-    int num; // The value of 'k' (the kth largest element we're interested in)
-    priority_queue<int, vector<int>, greater<int>> pq; // Min-heap to maintain the k largest elements
+    int num; // The value of 'k'
+    priority_queue<int, vector<int>, greater<int>> pq; // Min-heap of size k+1
 
 public:
     KthLargest(int k, vector<int>& nums) {
         num = k; // Initialize 'num' with the given value of 'k'
 
-        // Populate the min-heap with the first 'k' elements from 'nums'
+        // Populate the min-heap with the first k elements from 'nums'
         for (auto ele : nums) {
             pq.push(ele);
             if (pq.size() > k) {
-                pq.pop(); // If the heap size exceeds 'k', remove the smallest element
+                pq.pop(); // Maintain the heap size at k+1
             }
         }
     }
@@ -18,7 +18,7 @@ public:
     int add(int val) {
         pq.push(val); // Add the new element to the min-heap
         if (pq.size() > num) {
-            pq.pop(); // If the heap size exceeds 'k', remove the smallest element
+            pq.pop(); // Remove the smallest element (maintain heap size at k+1)
         }
         return pq.top(); // Return the current kth largest element
     }

dc6e48c985a28359b6be7cf9042e9ee54581a1b7
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-08-18 19:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (79.84%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        // Create a min heap to store potential ugly numbers.
-        // We'll use a min heap so that we always extract the smallest element.
-        priority_queue<long long, vector<long long>, greater<long long>> min_heap;
+        // Initialize an array to store ugly numbers.
+        vector<int> arr(n + 1);
 
-        // Create a set to keep track of all encountered ugly numbers.
-        unordered_set<long long> ugly_set;
+        // Create pointers for factors 2, 3, and 5.
+        int i2, i3, i5;
+        i2 = i3 = i5 = 1;
 
-        // Start with the first ugly number (which is 1).
-        min_heap.push(1);
-        ugly_set.insert(1);
+        // The first ugly number is always 1.
+        arr[1] = 1;
 
-        // While we haven't found the nth ugly number:
-        while (n > 1) {
-            // Extract the smallest element from the heap.
-            long long x = min_heap.top();
-            min_heap.pop();
+        // Generate the remaining ugly numbers.
+        for (int i = 2; i <= n; i++) {
+            // Compute the potential ugly numbers by multiplying with factors.
+            int i2ug = arr[i2] * 2;
+            int i3ug = arr[i3] * 3;
+            int i5ug = arr[i5] * 5;
 
-            // Check if 2*x, 3*x, and 5*x are not already in the set.
-            // If not, add them to the set and push them onto the heap.
-            if (ugly_set.find(2 * x) == ugly_set.end()) {
-                min_heap.push(2 * x);
-                ugly_set.insert(2 * x);
-            }
-            if (ugly_set.find(3 * x) == ugly_set.end()) {
-                min_heap.push(3 * x);
-                ugly_set.insert(3 * x);
-            }
-            if (ugly_set.find(5 * x) == ugly_set.end()) {
-                min_heap.push(5 * x);
-                ugly_set.insert(5 * x);
-            }
+            // Find the minimum of the three potential ugly numbers.
+            int minug = min(i2ug, min(i3ug, i5ug));
 
-            // Decrement n.
-            n--;
+            // Store the minimum as the next ugly number.
+            arr[i] = minug;
+
+            // Increment the pointers for the factors.
+            if (minug == i2ug) i2++;
+            if (minug == i3ug) i3++;
+            if (minug == i5ug) i5++;
         }
 
-        // The last element remaining in the heap is our nth ugly number.
-        return min_heap.top();
+        // Return the nth ugly number.
+        return arr[n];
     }
 };

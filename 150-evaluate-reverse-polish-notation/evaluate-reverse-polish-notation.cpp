e814d2a48c284a8e3f65e7691c2a5962fdd4c5ce
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-08-23 19:04:24 +0530

    Time: 12 ms (22.23%) | Memory: 15.7 MB (47.17%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -6,6 +6,7 @@ public:
         stack<int> stk; // Stack to store intermediate results
         for (string str : tokens) {
             if (str == "+" || str == "-" || str == "*" || str == "/") {
+                // As soon as a operand is encountered, remove the last 2 operands operate on them and push
                 int val1 = stk.top(); stk.pop();
                 int val2 = stk.top(); stk.pop();
                 int result;
@@ -14,9 +15,9 @@ public:
                 else if (str == "*") result = val2 * val1;
                 else if (str == "/") result = val2 / val1;
                 stk.push(result);
-            } else {
+            } else 
                 stk.push(stoi(str)); // Convert operand to integer and push
-            }
+            
         }
         return stk.top(); // Final result
     }

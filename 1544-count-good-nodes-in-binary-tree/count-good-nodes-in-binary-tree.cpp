a3dd16cefbb12022be7d335c364d3355bb760fb6
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-08-29 01:18:23 +0530

    Time: 88 ms (85.72%) | Memory: 85 MB (17.57%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int count = 0; // Count of good nodes
 
     int goodNodes(TreeNode* root) {
-        dfs(root, INT_MIN);
+        int count=0; // count number of good nodes
+        dfs(root, INT_MIN, count);
         return count;
     }
 
 private:
-    void dfs(TreeNode* node, int maxSoFar) {
+    void dfs(TreeNode* node, int maxSoFar, int &count) {
         if (!node) {
             return; // Base case: nullptr node
         }
@@ -21,8 +21,8 @@ private:
         maxSoFar = max(maxSoFar, node->val);
 
         // Recurse on left and right subtrees
-        dfs(node->left, maxSoFar);
-        dfs(node->right, maxSoFar);
+        dfs(node->left, maxSoFar, count);
+        dfs(node->right, maxSoFar, count);
     }
 
 };

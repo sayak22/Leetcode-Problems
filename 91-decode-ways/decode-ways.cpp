fc8392931114687afc4e5df16edb9f3b3cc1d1be
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-08-30 03:40:00 +0530

    Time: 5 ms (13.18%) | Memory: 15.4 MB (5.06%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -4,19 +4,23 @@ public:
     int solve(int i, string s, vector<int>& dp) {
         // base cases
 
+        // When we have gone out of bounds of the string, it means
+        // that we have decoded the entire string, and therefore
+        // we return 1
         if (i >= s.size())
-            return 1; // When we have gone out of bounds of the string, it means
-                      // that we have decoded the entire string, and therefore
-                      // we return 1
-        else if (s[i] == '0')
-            return 0; // We neither can decode 0 individually, neither with
-                      // another character like 06, therefore return 0 whenever
-                      // we encounter 0
+            return 1;
 
+        // We neither can decode 0 individually, neither with
+        // another character like 06, therefore return 0 whenever
+        // we encounter 0
+        else if (s[i] == '0')
+            return 0;
+
+        // when we reach the last character that means there can
+        // only be one way to decode one character, therefore
+        // return 1.
         else if (i == s.size() - 1)
-            return 1; // when we reach the last character that means there can
-                      // only be one way to decode one character, therefore
-                      // return 1.
+            return 1;
         // body
         else if (dp[i] != -1)
             return dp[i]; // check if computed earlier or not

87e471e9a2a665ef3e1e25266f0d805a45c74ec3
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-08-30 03:35:52 +0530

    Time: 4 ms (33.15%) | Memory: 15.7 MB (5.06%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-vector<int> dp;
+
     //RECCURSION AND MEMORISATION
-    int solve(int i, string s){
+    int solve(int i, string s, vector<int> &dp){
         //base cases
-        if(i >= s.size()) return 1;
-        else if(s[i]=='0') return 0;
-        else if(i==s.size()-1) return 1;
+        
+        if(i >= s.size()) return 1; // When we have gone out of bounds of the string, it means that we have decoded the entire string, and therefore we return 1
+        else if(s[i]=='0') return 0;// We neither can decode 0 individually, neither with another character like 06, therefore return 0 whenever we encounter 0
+        
+        else if(i==s.size()-1) return 1;//when we reach the last character that means there can only be one way to decode one character, therefore return 1.
         //body
-        else if(dp[i]!=-1) return dp[i];
-        else if(s[i]=='1' or (s[i]=='2' and s[i+1]<='6'))
-        return dp[i]= solve(i+1,s) + solve(i+2,s);
+        else if(dp[i]!=-1) return dp[i];// check if computed earlier or not
+
+        else if(s[i]=='1' or (s[i]=='2' and s[i+1]<='6')) 
+        return dp[i]= solve(i+1,s,dp) + solve(i+2,s,dp); // if the i and i+1 th number is between 10  to 26 then this is executed, i.e, decode taking i th character, or taking i and i+1 th character together, and decode the rest 
         else
-        return dp[i]=solve(i+1,s);
+        return dp[i]=solve(i+1,s,dp); // else just take a single character and decode the rest
 
     }
 
     int numDecodings(string s) {
-        dp.resize(s.size());
-        dp.assign(s.size(),-1);
-          return solve(0,s);
+        vector<int> dp(s.size(),-1);
+          return solve(0,s,dp);
     }
 };

da2432e844ab21e8a96f458825779084798b647a
Sayak Mondal <81412149+sayak22@users.noreply.github.com>
2024-09-02 13:58:27 +0530

    Time: 87 ms (85.34%) | Memory: 76.9 MB (29.23%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long long int totalChalks=0;
-        for(int it: chalk)
-            totalChalks+=it;
-        
-        k%=totalChalks;
+        long long int totalChalksReqd = 0;
 
-        for(int i=0;i<chalk.size();i++){
-            if(chalk[i]>k) return i;
-            else k-=chalk[i];
+        // finding total chalks required for the entire class.
+        for (int it : chalk)
+            totalChalksReqd += it;
+
+        // finding out the remainder of chalks left after doing multiple rounds
+        // of using 'totalChalksReqd' by the entire class
+        k %= totalChalksReqd;
+
+        // now finding which student will fall short of chalks to write
+        for (int i = 0; i < chalk.size(); i++) {
+            if (chalk[i] > k)
+                return i;
+            else
+                k -= chalk[i];
         }
+
         return -1;
     }
 };
